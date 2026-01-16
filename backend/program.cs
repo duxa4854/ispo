8d10161fd8f6add0271161e7a5a23e29ed8ddc54
gggg <gggg@gmail.com>
2026-01-16 13:48:17 +0300

    добавлен лог Git

--- backend/program.cs
+++ backend/program.cs
@@ -1,5 +1,21 @@
-﻿Console.WriteLine("Hello, World!");
-Console.WriteLine("Меня зовут Заиченко А.А");
+﻿Console.Write("Введите ваше имя: ");
+string name = Console.ReadLine();
+Console.WriteLine($"Привет, {name}!");
+Console.WriteLine($"Сегодня:");
 Console.WriteLine(DateTime.Now);
-Console.WriteLine(DateTime.Now);
-// Console.WriteLine(DateTime.Now);
+Console.Write("Хотите продолжить? (y/n): ");
+string answer = Console.ReadLine();
+if (answer == "y" || answer == "yes" || answer == "да")
+{
+    Console.WriteLine("Отлично! Продолжаем...");
+}
+else if (answer == "n" || answer == "no" || answer == "нет")
+{
+    Console.WriteLine("До свидания!");
+}
+else
+{
+    Console.WriteLine("Неизвестный ответ. Завершаем работу.");
+}
+Console.WriteLine("\nНажмите любую клавишу для выхода...");
+Console.ReadKey();
