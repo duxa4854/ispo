ae3b48c6ce34fc0676433277965e79e842262b71
gggg <gggg@gmail.com>
2026-01-16 11:08:38 +0300

    Used VS Code hotkeys

--- backend/program.cs
+++ backend/program.cs
@@ -1,3 +1,5 @@
 ﻿Console.WriteLine("Hello, World!");
 Console.WriteLine("Меня зовут Заиченко А.А");
 Console.WriteLine(DateTime.Now);
+Console.WriteLine(DateTime.Now);
+// Console.WriteLine(DateTime.Now);
